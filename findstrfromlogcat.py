d791cb877c09a218b5bb6ec1d83b31b8459e86c1
yuweixx <yuweixx@126.com>
2015-11-15 12:51:36 +0800

    update1

--- findstrfromlogcat.py
+++ findstrfromlogcat.py
@@ -6,23 +6,31 @@ import sys
 import datetime,time
 import linecache
 
-logcatfilepath=''
-logname=''
+logcatfilepath = os.path.split(os.path.realpath(__file__))[0]
+logcatname=logcatfilepath+'\\logcat.log'
 #1.获取文件行号
 def filerowsum(logname):
-	rowsum=len(open(logcatfilepath,logname).readlines())
+	rowsum=len(open(logcatname,'rU').readlines())
 	return rowsum
 #2.查找每一行是否有字符
 def seachstr(strname,rownum):
-	str1= linecache.getline(logname,rowsum)
-	if str1.find(strname)!=null:
+	str1= linecache.getline(logcatname,rownum)
+	
+	if str1.find(strname)!=null :
+		print('True')
 		return True
 	else :
+		print('False')
 		return False
 #3.循环查找，然后打印某行
+#str1=input()
+str1='activity'
 n=0
-while n<rowsum:
-	count = linecache.getline(filename,linenum)
-	if seachstr(count,n)=True:
+rSum=filerowsum(logcatname)
+while n<rSum:
+	count = linecache.getline(logcatname,n)
+	print count
+	if seachstr(str1,n)==True:
+		print n
 		print count
 	n=n+1
